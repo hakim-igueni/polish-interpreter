7fb4c1e03c17a4d88ebfb398fce30ffbfc624d3c
Hakim IGUENI <hakim.igueni@etu.u-paris.fr>
2021-12-25 21:51:44 +0100

    Ajouter a fonction print_polish

--- polish.ml
+++ polish.ml
@@ -143,22 +143,26 @@ read_expr ["+";"x";"2";"<";"h"];;
 
 let rec read_instr (pos: position) (niv : int) (lines : (position * string) list) : (position * instr * (position * string) list) =
   match lines with 
-  |[] -> failwith "Liste vide"
+  |[] -> failwith "Programme vide"
   |x::xs -> 
     match x with p, s ->
-      if String.length (String.trim s) = 0 then read_instr pos niv xs (*Ignorer les lignes vides ou ne contenant que des blancs*)
+      if String.length (String.trim s) = 0 then read_instr (pos+1) niv xs (*Ignorer les lignes vides ou ne contenant que des blancs*)
       else 
       let nb_ind = nb_indentations s
       in if nb_ind mod 2 <> 0 then failwith "Erreur de syntaxe: nombre d'indentations impair"
       else if (nb_ind/2) <> niv then failwith "Erreur de syntaxe: nombre d'indentations non respecté"  
-      else let mots = create_mots s in match mots with 
+      else let mots = create_mots s in (match mots with 
         |[] -> failwith "Erreur de syntaxe:?????" (* ligne vide *)
         |y::ys -> match y with
-          |"COMMENT" -> read_instr pos niv xs (* pourquoi pas pos+1 *)
-          |"READ" -> (match ys with 
-            |[v] -> (match (int_of_string_opt v) with
-            | Some n -> failwith "Erreur de syntaxe: READ ne supporte pas un entier en paramètre"
-            | None -> (pos, Read (v), xs)) (*(pos, Read (v), xs) *)
+          |"COMMENT" -> read_instr (pos+1) niv xs (* pourquoi pas pos+1 *)
+          |"READ" -> (match ys with   
+            |[v] -> 
+              if int_of_string_opt v = None && operateur v = None then 
+                begin
+                  (*let env = NameTable.add v v env;*)
+                  (pos, Read (v), xs)
+                end
+              else failwith "Erreur de syntaxe: le paramètre de READ doit être un nom de variable"
             |_-> failwith "Erreur de syntaxe: READ ne supporte pas plus d'un paramètre")
           |"PRINT" -> let exp, reste = read_expr ys in 
                       if reste = [] then (pos, Print (exp), xs)
@@ -180,7 +184,7 @@ let rec read_instr (pos: position) (niv : int) (lines : (position * string) list
               |_ -> (Set (y, read_expr zs), xs)* failwith "TODO"*)
               else let exp, reste = read_expr zs in 
               if reste = [] then (pos, Set(y, exp), xs)
-              else failwith "Erreur de syntaxe: On peut pas affecter plus d'une expression")
+              else failwith "Erreur de syntaxe: On peut pas affecter plus d'une expression") : position * instr * (position * name) list)
 
 and read_block (pos: position) (niv : int) (lines : (position * string) list) : (position * block * (position * string) list) =
   match lines with 
@@ -190,7 +194,7 @@ and read_block (pos: position) (niv : int) (lines : (position * string) list) : 
     in let (new_pos2, bloc, reste2) = read_block (new_pos1+1) niv reste1
     in (new_pos2, ((pos, instruction)::bloc), reste2);;
 
-read_block 1 0 [(1, "IF x = + + 3 4 5");(2, "  READ n");(3, "  PRINT n")];;
+read_instr 1 0 [(1, "IF x = 7");(2, "  PRINT n")];;
 let read_program (lines : (position * string) list) : program =
   match lines with 
   | [] -> failwith "Programme vide"
@@ -205,41 +209,108 @@ let read_polish (filename:string) : program =
   else read_program lines;;
 
 
-let print_polish (p:program) : unit = failwith "TODO"
+let print_ind (ind:int) : unit =
+  print_string(String.make ind ' ')
+
+let print_op (opr : op) : unit = 
+  match opr with
+  |Add -> print_string " + "
+  |Sub -> print_string " - "
+  |Mul -> print_string " * "
+  |Div -> print_string " / "
+  |Mod -> print_string " % "
+
+let rec print_expr (expr : expr) : unit =
+  match expr with 
+  | Num(v) -> print_int(v)
+  | Var(v) -> print_string(v)
+  | Op(op,expr1,expr2) -> print_expr(expr1);print_op (op);print_expr(expr2)
+
+let print_cond (cond : cond) : unit =
+  match cond with 
+  |(exp1,comp,exp2)-> 
+    print_expr(exp1);
+    (match comp with
+    |Eq -> print_string " = "
+    |Ne -> print_string " <> "
+    |Lt -> print_string " < "
+    |Le -> print_string " <= "
+    |Gt -> print_string " > "
+    |Ge -> print_string " >= ");
+    print_expr(exp2)
+
+let rec print_instr (inst : instr) (ind:int) : unit =
+  match inst with
+  | Set (v,expr) -> print_string(v ^ " := " );print_expr(expr);print_newline()
+  | Read (v) -> print_string("READ "^v);print_newline()
+  | Print(expr) -> print_string("PRINT ");print_expr(expr);print_newline()
+  | If (cond,block1,block2) -> print_string("IF ");print_cond(cond);print_program block1 (ind+2);print_program block2 (ind+2)
+  | While(cond,block) -> print_string("WHILE ");print_cond(cond);print_newline();print_program block (ind+2)
+and print_program (p:program) (ind : int) : unit =
+  match p with 
+  |[] -> ()
+  |(pos,instr)::ps -> print_ind ind; print_instr instr ind;print_program ps ind
+
+let print_polish (p:program) : unit = 
+    print_newline();
+    print_program p 0;;
+  
+
+print_polish [(2, Read "n"); (3, Set ("i", Num 1)); (4, Set ("r", Num 1));
+(6,
+ While ((Var "i", Le, Num 5),
+  [(7, Set ("r", Op (Mul, Var "i", Var "r")));
+   (8, Set ("r", Op (Mul, Var "i", Var "r")));
+   (9,
+    While ((Var "i", Le, Num 8),
+     [(10, Set ("i", Op (Add, Var "i", Num 1)));
+      (11, Set ("i", Op (Add, Var "i", Num 1)))]));
+   (12, Print (Var "r"))]));
+(13, Print (Var "i"))];;
 
 
-let op_to_string op = 
-  match op with
-  |Add -> print_string "+ "
-  |Sub -> print_string "- "
-  |Mul -> print_string "* "
-  |Div -> print_string "/ "
-  |Mod -> print_string "% "
-let get_n n env = 
-  try Hashtbl.find env n with
+let get_value v = 
+  try NameTable.find v env with
   Not_found -> failwith ""
-let eval_expr (exp: expr) (envir : env) = 
+
+let rec eval_expr (exp: expr) =
   match exp with 
   |Num (n) -> n
-  |Var (v) -> get_n v envir
+  |Var (v) -> get_value v
   |Op (op, expr1, expr2) -> (match op with 
-    |Add -> (eval_expr expr1 envir) + (eval_expr expr2 envir)
-    |Sub -> (eval_expr expr1 envir) - (eval_expr expr2 envir)
-    |Mul -> (eval_expr expr1 envir) * (eval_expr expr2 envir)
-    |Div -> if (eval_expr expr2 envir) <> 0 then (eval_expr expr1 envir) / (eval_expr expr2 envir)
+    |Add -> (eval_expr expr1) + (eval_expr expr2)
+    |Sub -> (eval_expr expr1) - (eval_expr expr2 )
+    |Mul -> (eval_expr expr1) * (eval_expr expr2)
+    |Div -> if (eval_expr expr2) <> 0 then (eval_expr expr1) / (eval_expr expr2)
       else failwith "Division par zero"
-    |Mod -> if (eval_expr expr2 envir) <> 0 then (eval_expr expr1 envir) mod (eval_expr expr2 envir)
+    |Mod -> if (eval_expr expr2) <> 0 then (eval_expr expr1) mod (eval_expr expr2)
       else failwith "Modulo par zero")
-    |_ -> failwith ""
-let eval_cond (cond:cond) (envir:env) =
+  |_ -> failwith ""
+let eval_cond (cond:cond) =
   match cond with
-  |(expr1, comp, expr2) -> match comp with
-    | Eq -> (eval_expr expr1 envir) = (eval_expr expr2 envir)
-    | Ne -> (eval_expr expr1 envir) <> (eval_expr expr2 envir)
-    | Lt -> (eval_expr expr1 envir) < (eval_expr expr2 envir)
-    | Le -> (eval_expr expr1 envir) <= (eval_expr expr2 envir)
-    | Gt -> (eval_expr expr1 envir) > (eval_expr expr2 envir)
-    | Ge -> (eval_expr expr1 envir) >= (eval_expr expr2 envir)
+  |(expr1, comp, expr2) -> (match comp with
+    | Eq -> (eval_expr expr1) = (eval_expr expr2 )
+    | Ne -> (eval_expr expr1) <> (eval_expr expr2)
+    | Lt -> (eval_expr expr1) < (eval_expr expr2)
+    | Le -> (eval_expr expr1) <= (eval_expr expr2)
+    | Gt -> (eval_expr expr1) > (eval_expr expr2)
+    | Ge -> (eval_expr expr1) >= (eval_expr expr2)
+    |_ -> failwith "")
+  |_ -> failwith ""
+
+
+let eval_block block = 
+  ;;
+let 
+let eval_instr (instruction : instr) = 
+  match instruction with 
+  | Set (v,expr) -> let env = NameTable.add v (eval_expr expr) env
+  | Read (n) -> begin Printf.printf "%s = " n; let v = read_int() in let env = NameTable.add n v env; end
+  | Print (expr) -> Printf.printf "%s" (eval_expr expr)
+  | If (cond,block1,block2) -> if eval_cond cond then eval_block block1 else eval_block2
+  | While (cond,block) -> eval_while cond block
+  |_ -> failwith ""
+
 
 let eval_polish (p:program) : unit = failwith "TODO"
 
@@ -255,4 +326,4 @@ let main () =
 
 (* lancement de ce main *)
 let () = main ()
-int_of_string "9o"
+int_of_string "9o";;
